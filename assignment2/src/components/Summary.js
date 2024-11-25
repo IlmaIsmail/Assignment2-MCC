dba94e3ce9557761649b00dad7ddd99a664d97be
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-25 23:01:37 +0000

    setting summary

---
+++ assignment2/src/components/Summary.js
@@ -0,0 +1,15 @@
+import '../index.css'
+
+export default function Summary({students, calculateAverage}){
+  
+  return (
+    <>
+      <div className='summary'>
+        <h3>Summary:</h3>
+        <p>Total Students: {students.length}</p>
+        <p>Average Age: {calculateAverage(students, 'age')}</p>
+        <p>Average GPA: {calculateAverage(students, 'gpa')}</p>
+      </div>
+    </>
+  );
+}
